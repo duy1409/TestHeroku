3a3e9d46015ff194dd51c12364b918a68b5c6a16
Nguyen Duong Duy <duyndgch17333@fpt.edu.vn>
2019-08-29 15:53:49 +0700

    xz
    zx

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
-<head>
-	<title>Hello Heroku</title>
-</head>
-<body>
-<?php
-echo "<h1>Hello Heroku PHP!!!<h1>";
-?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
 </html>
